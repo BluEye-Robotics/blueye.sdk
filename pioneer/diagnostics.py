89febc4a7b3334cb7da5033d53ebecb95edceedf
Johannes Schrimpf <johannes.schrimpf@blueye.no>
2019-08-12 12:33:43 +0200

    Initial commit

---
+++ pioneer/diagnostics.py
@@ -0,0 +1,51 @@
+import json
+import urllib
+import urllib.error
+import urllib.request
+import json
+import http.client
+
+
+def get_diagnostic_data(ip, name, port=80, post_data=None, method=None):
+    data = get_website_json("http://%s:%d/diagnostics/%s" % (ip, port, name), post_data, method=method)
+    return data
+
+
+def get_website_json(url, post_data=None, method=None):
+    retries = 0
+    while retries < 3:
+        data_json = get_website(url, post_data, method=method)
+        if data_json is not None:
+            break
+        retries += 1
+        print("No data, retriying")
+    if data_json is None:
+        return None
+    try:
+        data = json.loads(data_json)
+    except json.decoder.JSONDecodeError:
+        print("Could not decode json")
+        return None
+    return data
+
+def get_website(url, post_data=None, method=None, decode_utf8=True):
+    try:
+        if method is not None:
+            url = urllib.request.Request(url, method=method)
+        if post_data is not None:
+            post_data = urllib.parse.urlencode(post_data).encode('utf-8')
+            data = urllib.request.urlopen(url, post_data).read()
+        else:
+            data = urllib.request.urlopen(url).read()
+        if decode_utf8:
+            data = data.decode("utf-8")
+    except ConnectionRefusedError:
+        print("Could not connect to server")
+        return None
+    except urllib.error.URLError:
+        print("Could not connect to server")
+        return None
+    except http.client.RemoteDisconnected:
+        print("RemoteDisconnect")
+        return None
+    return data
---
+++ pioneer/pioneer.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import cv2
+import pprint
+#import threading
+
+from diagnostics import get_diagnostic_data
+from video import get_image, save_image
+from p2_app_protocol import UdpClient
+
+class Pioneer:
+    def __init__(self, ip="192.168.1.101"):
+        self._ip=ip
+        #self._cached_pioneer_state = None
+        #self._pioneer_state_event = threading.Event()
+        self._udpclient = UdpClient()
+
+    @property
+    def _pioneer_state(self):
+        #self._pioneer_state_event.wait()
+        #return self._cached_pioneer_state
+        return self._udpclient.get_data_dict()
+
+    
+    #def _update_pineer_state(self, state):
+    #    self._last_pioneer_state = state
+    #    self._pioneer_state_event.set()
+
+    def set_lights(self):
+        pass
+
+    @property
+    def depth(self):
+        return self._pioneer_state["depth"]
+
+    @property
+    def drone_info(self):
+        return get_diagnostic_data(self._ip, "drone_info")
+
+    def print_drone_info(self):
+        pprint.pprint(self.drone_info)
+
+    def get_image(self):
+        get_image(ip=self._ip)
+
+    def save_image(self, filename):
+        save_image(ip=self._ip, filename=filename)
+
+
+if __name__ == "__main__":
+    pioneer = Pioneer()
+    pioneer.print_drone_info()
+    pioneer.save_image("test.png")
+    print(pioneer.depth)
+    pioneer.show_video()
