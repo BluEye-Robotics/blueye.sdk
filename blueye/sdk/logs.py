e921f552d45e99ce3e7e09422b98927153ff9287
Sindre Hansen <sindrehansen92@gmail.com>
2019-10-22 13:34:25 +0200

    Add docstrings

--- blueye/sdk/logs.py
+++ blueye/sdk/logs.py
@@ -3,6 +3,27 @@ from datetime import datetime
 
 
 class LogFile:
+    """
+    This class is a container for a log file stored on the drone
+
+    The drone lists the file name, max depth, start time, and file size for each log,
+    and you can show this information by printing the log object, eg. on a Pioneer
+    object called `p`:
+
+    ```
+    print(p.logs[0])
+    ```
+
+    or, if you want to display the header you can format the object with `withHeader`:
+
+    ```
+    print(f{p.logs[0]:withHeader})
+    ```
+
+    Calling the download() method on a log object will pull the CSV (Comma Separated
+    Value) file from the drone to your local filesystem.
+    """
+
     def __init__(self, maxdepth, name, timestamp, binsize, ip):
         self.maxdepth = maxdepth
         self.name = name
@@ -20,6 +41,15 @@ class LogFile:
         return "%.1f %s%s" % (num, "Gi", suffix)
 
     def download(self, outputPath=None, outputName=None):
+        """
+        Download the specified log to your local file system
+
+        If you specify an outputPath the log file will be downloaded to that directory
+        instead of the current one.
+
+        Specifying outputName will overwrite the default file name with whatever you
+        have specified (be sure to include the .csv extension).
+        """
         log = requests.get(self.downloadPath).content
         if outputPath is None:
             outputPath = "./"
@@ -47,6 +77,24 @@ class LogFile:
 
 
 class Logs:
+    """This class is an index of the log files stored on the drone
+
+    To show the available logs you simply print this object, ie. if your Pioneer object
+    is called `p`, you can do:
+
+    ```
+    print(p.logs)
+    ```
+
+    This will print a list of all available logs, with some of their metadata, such as
+    name and maxdepth.
+
+    You can access logfile objects either by index or by name. Eg. if you want the first
+    logfile in the list you can do `p.logs[0]`, or if you want some particular log you
+    can do `p.logs["exampleName0001.csv"]`. You can even give it a slice, so if you want
+    the last 10 logs you can do `p.logs[:-10]`.
+    """
+
     def __init__(self, ip="192.168.1.101"):
         self.ip = ip
         listOfLogsInDictionaries = self.getListOfLogsFromDrone()
