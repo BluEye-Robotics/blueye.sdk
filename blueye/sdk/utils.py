9f57e1f40ce1825df0a2e807c237b13931a5b916
Sindre Hansen <sindrehansen92@gmail.com>
2019-11-15 12:44:39 +0100

    Add utility function for opening local docs

---
+++ blueye/sdk/utils.py
@@ -0,0 +1,16 @@
+import webbrowser
+import os
+import blueye.sdk
+
+
+def open_local_documentation():
+    """Open a pre-built local version of the SDK documentation
+
+    Useful when you are connected to the drone wifi, and don't have access to the online version.xs
+    """
+    sdk_path = os.path.dirname(blueye.sdk.__file__)
+
+    # The documentation is located next to the top-level package so we move up a couple of levels
+    documentation_path = sdk_path + "/../../blueye.sdk_docs/README.html"
+
+    webbrowser.open(documentation_path)
