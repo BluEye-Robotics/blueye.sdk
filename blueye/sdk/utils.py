c4d5a22501793d7d28c216c5b706c47cb11f3025
Sindre Hansen <sindrehansen92@gmail.com>
2019-11-15 14:00:08 +0100

    Fix typo

--- blueye/sdk/utils.py
+++ blueye/sdk/utils.py
@@ -6,7 +6,7 @@ import blueye.sdk
 def open_local_documentation():
     """Open a pre-built local version of the SDK documentation
 
-    Useful when you are connected to the drone wifi, and don't have access to the online version.xs
+    Useful when you are connected to the drone wifi, and don't have access to the online version.
     """
     sdk_path = os.path.dirname(blueye.sdk.__file__)
 
