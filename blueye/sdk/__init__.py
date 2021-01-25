cdd3abcbbc7b2a34b7e971d5045eb1a8305dac8d
Sindre Hansen <sindrehansen92@gmail.com>
2021-01-25 11:48:31 +0100

    Add function for controlling logo overlay

--- blueye/sdk/__init__.py
+++ blueye/sdk/__init__.py
@@ -1,3 +1,4 @@
+from .camera import LogoOverlay
+from .constants import WaterDensities
 from .drone import Drone, Pioneer
 from .utils import open_local_documentation
-from .constants import WaterDensities
