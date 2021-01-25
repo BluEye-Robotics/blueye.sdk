0af5358c34c9850a65b8ad6ee1743a9cacb9afc1
Sindre Hansen <sindrehansen92@gmail.com>
2021-01-25 14:31:12 +0100

    Add function for setting depth unit in overlay

--- blueye/sdk/__init__.py
+++ blueye/sdk/__init__.py
@@ -1,4 +1,4 @@
-from .camera import LogoOverlay
+from .camera import DepthUnitOverlay, LogoOverlay
 from .constants import WaterDensities
 from .drone import Drone, Pioneer
 from .utils import open_local_documentation
