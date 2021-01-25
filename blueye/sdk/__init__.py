a9a611a8445592636c69fd1f4e9048793329bf4b
Sindre Hansen <sindrehansen92@gmail.com>
2021-01-25 20:47:06 +0100

    Add function for setting font size in overlay

--- blueye/sdk/__init__.py
+++ blueye/sdk/__init__.py
@@ -1,4 +1,4 @@
-from .camera import DepthUnitOverlay, LogoOverlay, TemperatureUnitOverlay
+from .camera import DepthUnitOverlay, FontSizeOverlay, LogoOverlay, TemperatureUnitOverlay
 from .constants import WaterDensities
 from .drone import Drone, Pioneer
 from .utils import open_local_documentation
