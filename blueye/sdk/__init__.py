39fca0f2918edfbdebd49b59cb21dfd231bb7a9c
Sindre Hansen <sindrehansen92@gmail.com>
2023-03-01 12:21:33 +0100

    Update overlay properties to v3 protocol

--- blueye/sdk/__init__.py
+++ blueye/sdk/__init__.py
@@ -1,4 +1,3 @@
-from .camera import DepthUnitOverlay, FontSizeOverlay, LogoOverlay, TemperatureUnitOverlay
 from .constants import WaterDensities
 from .drone import Drone
 from .utils import open_local_documentation
