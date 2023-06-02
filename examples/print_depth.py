167711732e6491bdd215b87384e8d0814ec6d614
Sindre Hansen <sindrehansen92@gmail.com>
2023-06-02 11:46:40 +0200

    Add documentation for subscribing to telemetry

--- examples/print_depth.py
+++ examples/print_depth.py
@@ -1,3 +1,8 @@
+"""print_depth.py
+
+This example program demonstrates how one can add a callback function to a telemetry message, as
+well as how to adjust the frequency of that telemetry message, and how to remove the callback.
+"""
 import time
 
 import blueye.protocol as bp
@@ -5,14 +10,27 @@ import blueye.protocol as bp
 from blueye.sdk import Drone
 
 
-def callback_depth(msg_type, msg):
+def callback_depth(msg_type: str, msg: bp.DepthTel):
+    """Callback for the depth telemetry message
+
+    This function is called once for every DepthTel message received by the telemetry watcher
+    """
     print(f"Got a {msg_type} message with depth: {msg.depth.value:.3f}")
 
 
 if __name__ == "__main__":
+    # Instantiate a drone object
     my_drone = Drone()
+
+    # Add a callback for the DepthTel message, storing the ID for later use
     callback_id = my_drone.add_telemetry_msg_callback([bp.DepthTel], callback_depth)
+
+    # Adjust the publishing frequency to 5 Hz
     my_drone.set_telemetry_msg_publish_frequency(bp.DepthTel, 5)
 
+    # Callback is triggered by a separate thread while we sleep here
     time.sleep(5)
+
+    # Remove the callback using the ID we stored when it was created (not really necessary here
+    # since the my_drone object goes out of scope immediately afterwards)
     my_drone.remove_telemetry_msg_callback(callback_id)
