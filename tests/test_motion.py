2d1a37678bab4ca5cb6339dd430f4480badc9969
Sindre Hansen <sindrehansen92@gmail.com>
2024-05-28 14:20:26 +0200

    Add tests for auto heading and depth

--- tests/test_motion.py
+++ tests/test_motion.py
@@ -1,3 +1,6 @@
+import blueye.protocol as bp
+
+
 def test_boost_getter_returns_expected_value(mocked_drone):
     mocked_drone.motion._current_boost_setpoints["boost"] = 1
     assert mocked_drone.motion.boost == 1
@@ -19,3 +22,34 @@ def test_slow_setter_produces_correct_motion_input_arguments(mocked_drone):
     mocked_drone.motion.slow = slow_gain
     mocked_drone._ctrl_client.set_motion_input.assert_called_with(0, 0, 0, 0, slow_gain, 0)
 
+
+def test_auto_heading_returns_expected_value(mocked_drone):
+    mocked_drone._telemetry_watcher._state[bp.ControlModeTel] = bp.ControlModeTel.serialize(
+        bp.ControlModeTel(state=bp.ControlMode(auto_heading=True))
+    )
+    assert mocked_drone.motion.auto_heading_active is True
+
+
+def test_auto_heading_returns_none_on_missing_telemetry(mocked_drone):
+    assert mocked_drone.motion.auto_heading_active is None
+
+
+def test_auto_heading_produces_correct_control_message(mocked_drone):
+    mocked_drone.motion.auto_heading_active = True
+    mocked_drone._ctrl_client.set_auto_heading_state.assert_called_with(True)
+
+
+def test_auto_depth_returns_expected_value(mocked_drone):
+    mocked_drone._telemetry_watcher._state[bp.ControlModeTel] = bp.ControlModeTel.serialize(
+        bp.ControlModeTel(state=bp.ControlMode(auto_depth=True))
+    )
+    assert mocked_drone.motion.auto_depth_active is True
+
+
+def test_auto_depth_returns_none_on_missing_telemetry(mocked_drone):
+    assert mocked_drone.motion.auto_depth_active is None
+
+
+def test_auto_depth_production_correct_control_message(mocked_drone):
+    mocked_drone.motion.auto_depth_active = True
+    mocked_drone._ctrl_client.set_auto_depth_state.assert_called_with(True)
