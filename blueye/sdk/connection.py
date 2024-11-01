286d842bc4b4a6dde50960a9e6397c55cf1a3a0a
Sindre Hansen <sindrehansen92@gmail.com>
2024-11-01 10:02:08 +0100

    Add ctrl functions for starting/stopping missions

--- blueye/sdk/connection.py
+++ blueye/sdk/connection.py
@@ -383,6 +383,18 @@ class CtrlClient(threading.Thread):
         msg = blueye.protocol.LaserCtrl(laser={"value": intensity})
         self._messages_to_send.put(msg)
 
+    def run_mission(self):
+        msg = blueye.protocol.RunMissionCtrl()
+        self._messages_to_send.put(msg)
+
+    def pause_mission(self):
+        msg = blueye.protocol.PauseMissionCtrl()
+        self._messages_to_send.put(msg)
+
+    def clear_mission(self):
+        msg = blueye.protocol.ClearMissionCtrl()
+        self._messages_to_send.put(msg)
+
 
 class ReqRepClient(threading.Thread):
     """A thread that handles request-reply messages to and from the drone."""
