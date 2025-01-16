d920178bea685a2d1805975ff68e7aef9b287635
Sindre Hansen <sindrehansen92@gmail.com>
2025-01-16 14:24:53 +0100

    Add docstrings to the constants module

--- blueye/sdk/constants.py
+++ blueye/sdk/constants.py
@@ -5,7 +5,12 @@ This file contains constants used by the SDK.
 
 class WaterDensities:
     """
-    Various typical densities for salt water (in grams/liter)
+    Various typical densities for water (in grams/liter).
+
+    Attributes:
+        fresh (float): Density of fresh water.
+        brackish (float): Density of brackish water.
+        salty (float): Density of salty water.
     """
 
     fresh = 997.0
--- docs/reference/blueye/sdk/constants.md
+++ docs/reference/blueye/sdk/constants.md
@@ -1,1 +1,4 @@
 ::: blueye.sdk.constants
+    options:
+      summary: False
+
