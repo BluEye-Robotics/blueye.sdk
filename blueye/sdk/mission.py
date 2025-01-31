df1563db8cf6af028226976aacf8cfb1122a6cc7
Sindre Hansen <sindrehansen92@gmail.com>
2025-01-31 15:29:24 +0100

    Create a helper function to prepare missions

--- blueye/sdk/mission.py
+++ blueye/sdk/mission.py
@@ -1,9 +1,46 @@
+import copy
+import logging
+from typing import TYPE_CHECKING, List, Optional
+
 import blueye.protocol
-from typing import TYPE_CHECKING, Optional
 
 # Necessary to avoid cyclic imports
 if TYPE_CHECKING:
     from .drone import Drone
+
+logger = logging.getLogger(__name__)
+
+
+def prepare_new_mission(
+    instruction_list: List[blueye.protocol.Instruction],
+    mission_id: int = 0,
+    mission_name: str = "",
+) -> blueye.protocol.Mission:
+    """Creates a mission from a list of instructions
+
+    Automatically assigns an ID to each instruction based on the order they are in the list.
+
+    Args:
+        mission_id: ID of the mission
+        mission_name: Name of the mission
+        instruction_list: List of instructions to create the mission from
+
+    Returns:
+        A mission object with the instructions and their respective IDs
+    """
+    logger.debug(
+        f"Preparing the {mission_name} mission, with ID {mission_id} and "
+        f"{len(instruction_list)} instructions"
+    )
+    instruction_id = 0
+
+    # Deep copy the instructions to avoid modifying the original ones
+    for instruction in instruction_list:
+        instruction_copy = copy.deepcopy(instruction)
+        instruction_copy.id = instruction_id
+        instruction_id += 1
+        instruction_list[instruction_list.index(instruction)] = instruction_copy
+    return blueye.protocol.Mission(id=mission_id, name=mission_name, instructions=instruction_list)
 
 
 class Mission:
@@ -12,14 +49,18 @@ class Mission:
     The flow is a follows:
     1. Create some instructions
         import blueye.protocol as bp
-        go_to_seabed = bp.Instruction(go_to_seabed_command={"desired_speed": 0.5})
+        go_to_seabed = bp.Instruction(go_to_seabed_command={"desired_speed": 0.3})
         take_picture = bp.Instruction(camera_command={
             "camera_action": bp.CameraAction.CAMERA_ACTION_TAKE_PHOTO
         })
-        go_to_surface = bp.Instruction(go_to_surface_command={"desired_speed": 0.5})
+        go_to_surface = bp.Instruction(go_to_surface_command={"desired_speed": 0.3})
 
     2. Create a mission with the instructions
-        mission = bp.Mission(instructions=[go_to_seabed, take_picture, go_to_surface])
+        from blueye.sdk.mission import prepare_new_mission
+        mission = prepare_new_mission(
+            instruction_list = [go_to_seabed, take_picture, go_to_surface],
+            mission_id = 0,
+            mission_name = "Go to seabed and take a picture",)
 
     3. Clear any previous missions
         drone.mission.clear()
