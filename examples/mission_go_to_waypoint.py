3fcf560090e8c3c38805bcf46659d2d34f7787e9
Sindre Hansen <sindrehansen92@gmail.com>
2025-06-04 15:27:53 +0200

    Add examples for going to waypoints

---
+++ examples/mission_go_to_waypoint.py
@@ -0,0 +1,47 @@
+import logging
+import logging.handlers
+import time
+
+import blueye.protocol as bp
+
+from blueye.sdk import Drone
+from blueye.sdk.mission import create_waypoint_instruction, prepare_new_mission
+
+# Publish runtime logs over TCP (including mission status notifications)
+logger = logging.getLogger("blueye.sdk")
+logger.setLevel(logging.DEBUG)
+socket_handler = logging.handlers.SocketHandler(
+    "localhost", logging.handlers.DEFAULT_TCP_LOGGING_PORT
+)
+logger.addHandler(socket_handler)
+
+# Create some waypoints in a (roughly) square pattern
+#    10.3840°E           10.3841°E
+#    B ------------------ C 63.4612°N
+#    |                    |
+#    |                    |
+#    |                    |
+#    A ------------------ D 63.4610°N
+wp_a = create_waypoint_instruction("Point A", 63.4610, 10.3840, 10)
+wp_b = create_waypoint_instruction("Point B", 63.4612, 10.3840, 10)
+wp_c = create_waypoint_instruction("Point C", 63.4612, 10.3842, 10)
+wp_d = create_waypoint_instruction("Point D", 63.4610, 10.3842, 10)
+
+
+# Create a mission with the instructions
+mission = prepare_new_mission(
+    instruction_list=[wp_a, wp_b, wp_c, wp_d, wp_a],
+    mission_id=0,
+    mission_name="Go to waypoints",
+)
+
+# Establish a connection to the drone
+d = Drone(log_notifications=True)
+
+# Send the mission to the drone and start it
+d.mission.load_and_run(mission)
+
+# Wait until the mission state becomes MISSION_STATE_COMPLETED
+while d.mission.get_status().state != bp.MissionState.MISSION_STATE_COMPLETED:
+    time.sleep(0.1)
+print("Mission completed successfully!")
